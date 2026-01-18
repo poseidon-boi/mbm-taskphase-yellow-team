16c1fb37d3a40a02f036677c5ff6ae35992e92fc
halloween-bit16 <ruchi3110pawar@gmail.com>
2026-01-18 13:10:02 +0530

    lots of changes im losing my mind

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,14 +2,13 @@ function Footer() {
   return (
     <>
       <div className="bg-[#101317] text-[#C2C7CE] py-10 flex justify-center">
-        © 2025 Yellow Team for the taskphase of Manipal BioMachines. View our
-        source code{" "}
+        © 2025 Yellow Team for the taskphase of Manipal BioMachines. {" "}
         <a
           href="https://github.com/poseidon-boi/mbm-taskphase-yellow-team"
           target="_blank"
           className="underline ml-1"
         >
-          here
+          View source code here.
         </a>
         .
       </div>
